6b0a110cf2a04f91d8da878ea3837f4992b338f7
Chirag Jog <chirag.jog@gmail.com>
2013-01-04 15:03:36 +0530

    Add cookbook for hello world LAMP application.

---
+++ cookbooks/hello_app/attributes/default.rb
@@ -0,0 +1,5 @@
+default['hello_app']['db_user'] = 'hello_app'
+default['hello_app']['db_name'] = 'hello_app'
+default['hello_app']['server_name'] = 'hello_app.example.com'
+default['hello_app']['docroot'] = '/srv/hello_app/current'
+default['hello_app']['config_dir'] = '/srv/hello_app/shared/config'
---
+++ cookbooks/hello_app/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures hello_app"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends          "apache2"
+depends          "mysql"
